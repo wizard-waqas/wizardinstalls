d64c2981d68f8b25f783d9efd2fd4a57199f8c3b
waqasabbasi <waqaspathan00@gmail.com>
2025-02-01 02:41:14 -0500

    formatting

--- src/components/FAQSection/index.tsx
+++ src/components/FAQSection/index.tsx
@@ -4,10 +4,11 @@ import FAQItem from "@/components/FAQSection/FAQItem";
 export default function FAQSection() {
     return (
         <section id="faq-section" className="py-8 bg-grey-800">
-            <h3 className="text-center text-red-500 font-bold text-xl">
-                (FAQ) Frequently Asked Questions
+            <h3 className="text-center text-red-500 font-bold text-2xl">
+                FAQ
             </h3>
-            <div className="flex flex-col items-center mt-8">
+            <h3 className={"text-center mt-2"}>Frequently Asked Questions</h3>
+            <div className="flex flex-col items-center mt-4">
                 <div className="w-full md:w-1/2">
                     <FAQItem question="Are dashcams illegal?"
                              answer="Dashcams are legal in all 50 U.S. states. Our installation ensures it will not obstruct your view."/>
@@ -19,11 +20,11 @@ export default function FAQSection() {
                     {/*         answer="We can work out a deal for clients looking to install dash cams on more than one vehicle at once."/>*/}
                     {/*<FAQItem question="What happens if something goes wrong with my dashcam after installation"*/}
                     {/*         answer="We offer an installation warranty"/>*/}
-                    <FAQItem question="Are dashcam videos admissible in court or with insurance claims"
+                    <FAQItem question="Are dashcam videos admissible in court or with insurance claims?"
                              answer="This is where they are most helpful as they can help you get out of insurance fraud situations."/>
-                    <FAQItem question="Where do you provide your dashcam installation services"
+                    <FAQItem question="Where do you provide your dashcam installation services?"
                              answer="We are a mobile service, we come to you!"/>
-                    <FAQItem question="Cost of installing a dashcam"
+                    <FAQItem question="What is the cost of installing a dashcam?"
                              answer="For our service it is $70 for front dashcam and $120 for a rear dashcam. You can also see our [Dashcam Packages section](#dashcam-packages)"/>
                 </div>
             </div>
