dd017fd4784b5c0018395730a1d505195ff4bd0a
waqasabbasi <waqaspathan00@gmail.com>
2025-04-09 01:12:28 -0400

    added analytics

--- src/app/backup-cameras/page.tsx
+++ src/app/backup-cameras/page.tsx
@@ -1,11 +1,21 @@
 "use client";
 
-import React from "react";
+import React, {useEffect} from "react";
 import GetInTouchSection from "@/components/GetInTouchSection";
 import SplashSection from "@/components/BackupCameras/SplashSection";
 import SeeMoreSection from "@/components/SeeMoreSection";
+import {analytics} from "@/firebase";
+import {logEvent} from "@firebase/analytics";
 
 export default function BackupCamerasPage() {
+
+    useEffect(() => {
+        if (analytics) {
+            logEvent(analytics, "page_carplay_view", {
+                page_path: window.location.pathname,
+            });
+        }
+    }, []);
 
     return (
         <div id="dashcam-packages" className="bg-black">
